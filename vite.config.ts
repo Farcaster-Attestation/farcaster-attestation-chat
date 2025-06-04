d6d420ea0b4b90f7a4fdd3617563b2d378f93162
Chomtana <Chomtana001@gmail.com>
2025-06-04 22:00:05 +0700

    buffer polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,19 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    nodePolyfills({
+      include: ["buffer"],
+      globals: {
+        Buffer: true,
+      },
+      protocolImports: true,
+    }),
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
 });
